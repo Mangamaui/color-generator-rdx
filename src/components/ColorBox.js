5aa22467f67df166e4921ad3cf6904036ebc0b18
Claudia Reynders <exotic.maui@gmail.com>
2017-10-26 10:53:34 +0200

    updated React import

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 class ColorBox extends React.Component {
     render() {
--- src/components/ColorGroup.js
+++ src/components/ColorGroup.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ColorBox from './ColorBox';
 
 import COLOR from '../lib/color';
